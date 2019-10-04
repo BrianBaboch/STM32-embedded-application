9952c291d4ebb61d9971e674454b89ee7e22b7a1
Brian Baboch <brian.baboch@telecom-paris.fr>
2019-10-04 02:23:15 -0700

    Added comments

--- TD/led.c
+++ TD/led.c
@@ -10,29 +10,35 @@
 
 
 void led_init(){
-	//activate clock for for ports B and C
+	//Activate clock for for ports B and C
 	AHB2_RCC = AHB2_RCC | (3<<1);
+	//Puts PB14 in output mode
 	GPIOB_MODER = (GPIOB_MODER & ~(3<<28)) | (1<<28);
 }
 
 void led_g_on(){
+	//Sets 1 on the output of PB14
 	GPIOB_BSRR = GPIOB_BSRR | (1<<14);
 }
 
 void led_g_off(){
+	//Sets 0 on the output of PB14
 	GPIOB_BSRR = GPIOB_BSRR | (1<<30);
 }
 
 void led(int state){
 	if (state == LED_YELLOW){
+		//Puts PC9 in output mode and sets its output to 1 
 		GPIOC_MODER = (GPIOC_MODER & ~(3<<18)) | (1<<18);
 		GPIOC_BSRR = GPIOC_BSRR | (1<<9);
 	}
 	else if (state == LED_BLUE){
+		//Puts PC9 in output mode and sets its output to 1 
 		GPIOC_MODER = (GPIOC_MODER & ~(3<<18)) | (1<<18);
 		GPIOC_BSRR = GPIOC_BSRR | (1<<25);
 	}
 	else if (state == LED_OFF){
+		//Puts PC9 in input mode
 		GPIOC_MODER = (GPIOC_MODER & ~(3<<18));
 	}
 }
