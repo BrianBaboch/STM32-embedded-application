5788f50264502a99962fd41ce92f4bc3ca303c76
Brian Baboch <brian.baboch@telecom-paris.fr>
2019-10-21 07:02:44 -0700

    XIP working

--- TD/crt0.s
+++ TD/crt0.s
@@ -1,9 +1,10 @@
     .syntax unified
     .global _start 
-    .thumb
+    .thumb_func
 _start:
 	ldr sp, =_stack
 	bl init_bss
+	bl init_data	
     bl main
 _exit:
 	bl _exit
--- TD/init.c
+++ TD/init.c
@@ -1,6 +1,13 @@
 #include <stdint.h>
 
-extern uint8_t _bstart, _bend;
+extern uint8_t _bstart, _bend, _dstart, _dend, _roend;
+
+void init_data(){
+	uint8_t *src = &_roend;
+	uint8_t *dst = &_dstart;
+	while (dst < &_dend)
+ 		*dst++ = *src++;
+}
 
 void init_bss(){
 	uint8_t *dst = &_bstart;
